1932d044a16abe318e69f66dd594481d1550c514
azarovalex <alex@azarov.by>
2018-03-07 02:47:30 +0300

    Solution.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,123 @@
 // DO WHATEVER YOU WANT HERE
+const createEnumerableProperty = (property) => {
+    return property;
+};
+const createNotEnumerableProperty = (property) => {
+    Object.defineProperty(Object.prototype, 'property', {
+        value: 'value',
+        enumerable: false
+    })
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
-const asyncIncrementor = () => {};
-const createIncrementer = () => {};
+    return property;
+};
+const createProtoMagicObject = () => {
+    createProtoMagicObject.prototype = createProtoMagicObject.__proto__;
+    return createProtoMagicObject;
+};
+const incrementor = () => {
+    incrementor.value++;
+    return incrementor;
+};
+incrementor.value = 0;
+incrementor.valueOf = () => {
+    return incrementor.value;
+}
+
+
+const asyncIncrementor = () => {
+    asyncIncrementor.value++;
+    return asyncIncrementor.value;
+};
+asyncIncrementor.value = 0;
+const createIncrementer = () => {
+    var val = 0;
+    return {
+        next() {
+            return {
+                value: ++val,
+                done: false
+            };
+        },
+        [Symbol.iterator]() {
+            return this;
+        }
+    }
+};
 
 // return same argument not earlier than in one second, and not later, than in two
-const returnBackInSecond = () => {};
-const getDeepPropertiesCount = () => {};
-const createSerializedObject = () => {};
-const toBuffer = () => {};
-const sortByProto = () => {};
+const returnBackInSecond = (parameter) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            return resolve(parameter)
+        }, 1000);
+    })
+};
+const getDeepPropertiesCount = (obj) => {
+    var res = 0;
+
+    function _recursive(obj) {
+        var res = 0;
+        if (obj.length != 0) {
+            for (key in obj) {
+                res += 1;
+                if (Object.getOwnPropertyNames(obj[key]).length != 0) {
+                    res = res + _recursive(obj[key]);
+                }
+            }
+        } else {
+            res++;
+        }
+        return res;
+    }
+
+    for (key in obj) {
+        res += 1;
+        if (Object.getOwnPropertyNames(obj[key]).length != 0) {
+            res = res + _recursive(obj[key]);
+        }
+
+    }
+    return res;
+};
+const createSerializedObject = () => {
+    let serObj = {
+        key: "value"
+    };
+    Object.defineProperty(serObj, "toJSON", {
+        value: function () {
+            return serObj.toString();
+        }
+    });
+    return serObj;
+};
+
+const sortByProto = (arr) => {
+    var res = [];
+    for (let cur of arr) {
+
+        if (res.length == 0) {
+            res.push(cur);
+        } else {
+            if (cur.isPrototypeOf(res[0])) {
+                for (var j = 0; j < res.length; j++) {
+                    if (cur.isPrototypeOf(res[j])) {
+                        break;
+                    }
+                }
+                if (j == res.length) {
+                    res.push(cur);
+                } else {
+                    res.splice(j, 0, cur);
+                }
+
+            } else {
+                res.unshift(cur);
+            }
+        }
+    }
+
+    return res;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
